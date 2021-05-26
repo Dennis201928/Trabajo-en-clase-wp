19cb630107b94661853037575efea60e3f52e904
Dennis Quiguire <instagram.24@outlook.com>
2021-05-26 16:21:07 -0500

    Blog

--- front-page.php
+++ front-page.php
@@ -1,2 +1,118 @@
 <?php get_header(); ?>
+
+
+<body class="bg-gradient-to-r from-gray-500 ...">
+
+<center>
+<div class="text-5xl font-extrabold ..." >
+  <span class="bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500">
+    Mi bibliografia
+  </span>
+</div>
+</center>
+
+<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+  <div class="md:flex">
+    <div class="md:flex-shrink-0">
+      <img class="h-48 w-full object-cover md:h-full md:w-48" src=" <?php echo get_template_directory_uri(  )?> /assets/img/mio.png"  alt="Man looking at item at a store">
+    </div>
+    <div class="p-8">
+      <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">My life</div>
+      <a href="https://www.instagram.com/dviddennis/" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Mas sobre mi </a>
+      <p class="mt-2 text-gray-500">Quieres saber mas sobre mi dale al link :)</p>
+    </div>
+  </div>
+</div>
+
+
+<dir>
+<p style="color:#0800FC;">♦Mi nombre es Dennis Quiguire como vemos esta es mi primera pagina en wordpres lo cual me parece algo complicado pero 
+        a la ves me parece muy divertido de realizar otra cosas mas que me gusta ahcer el jugar baloncesto ya que es mi deporte favorito es el valoncesto
+ </p>
+</dir>
+
+<dir>
+<p class="text-black-600 ... " >
+    ♦Soy una persona puntual, responsable con todas las obligaciones encomendadas. Agilidad, eficacia y dinamismo para trabajar en equipo y bajo presión. Considero que el éxito de dirigir un grupo es
+    formar parte de él con respeto, sencillez y carisma.  
+ </p>
+</dir>
+
+<dir>
+<p style="color:#0800FC;">
+    ♦En periodos vacacionales trabaje como ayudante contable y mantenimiento de computadoras en una empresa de alquiler de maquinaria pesada.
+ </p>
+ </dir>
+
+<dir>
+<p class="text-black-600 ... ">
+    ♦Lo que relizo en mis practicas son por ejemplo pagos del sri pago s a los trabajadores ya que la empresa que trabajo 
+    se encarga de alquiler y renta de maquinarias .
+ </p>
+</dir>
+
+<dir>
+<p style="color:#0800FC;">
+ ♦Me considero alguien que consigue lo que se propone, ya que creo que los logros son resultados de las decisiones que se toman y las metas que cada uno se ponga”. O "como propósito intento 
+ mejorar lo que soy y lo que hago de manera constante", entre varias más.
+ </p>
+</dir>
+
+<dir>
+<p class="text-black-600 ... ">
+♦Considero bastante oportuno e interesante comenzar a trabajar en el ámbito en el que me he estado formando estos últimos años al realizar 
+mi carrera universitaria y, así, poder desempeñar todo lo que he aprendido
+ </p>
+ </dir>
+
+<dir  style="color:#0800FC;"> 
+♦Unos de mis musicas favoritas es esta ya que me permite trabajar y me concentro un poquito mas:
+<a href="https://www.youtube.com/watch?v=7wtfhZwyrcc" class="no-underline hover:underline ..."><p style="color:#FF01F7;">Dragons - Believer</p></a>
+</dir>
+
+<dir>
+<p class="text-black-600 ... ">
+♦Mi juego favorito por el momento es valortant ya que soy muy "bueno" jugando .
+ </p>
+ </dir>
+
+ <div class="w-5/12 rounded-full hover:shadow-2xl transition">
+            <img
+                class="rounded-full w-full"
+                src="<?php echo get_template_directory_uri() ?>/assets/img/valorant.jpg" 
+                
+            >
+        </div>
+ 
+
+        <dir>
+<p style="color:#0800FC;">
+♦Mi comida favorita es la pizza.
+ </p>
+ </dir>
+
+ <div class="w-5/12 rounded-full hover:shadow-2xl transition">
+            <img
+                class="rounded-full w-full"
+                src="<?php echo get_template_directory_uri() ?>/assets/img/pizza-hut.jpg" 
+                
+            >
+        </div>
+
+        <dir>
+
+<p  class="text-black-600 ... ">
+♦Mi pelicula es The Maze Runner.
+ </p>
+ </dir>
+
+ <div class="w-5/12 rounded-full hover:shadow-2xl transition">
+            <img
+                class="rounded-full w-full"
+                src="<?php echo get_template_directory_uri() ?>/assets/img/Maze-Runner.jpg" 
+                
+            >
+        </div>
+ 
+
 <?php get_footer(); ?>
